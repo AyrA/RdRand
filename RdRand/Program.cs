095541550a577aca004ab3238b143c533befb4e1
Kevin Gut <kevin.gut@kauz.ch>
2018-01-08 21:25:58 +0100

    Add argument handling

--- RdRand/Program.cs
+++ RdRand/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace RdRand
+﻿using System;
+using System.Linq;
+
+namespace RdRand
 {
     class Program
     {
@@ -6,16 +9,59 @@
         {
             public const int SUCCESS = 0;
             public const int NO_RDRAND = 1;
+            public const int HELP = 2;
+            public const int INVALID_ARGS = 3;
         }
+
+        public const int BUFFER = 1024 * 4;
+
 
         static int Main(string[] args)
         {
-            if (RdRand_Interop.IsRdRandSupported)
+#if DEBUG
+            args = new string[] { "1024" };
+#endif
+
+            if (args.Contains("/?"))
             {
-                byte[] data = RdRand_Interop.GetRandom(1024);
-                return EXIT.SUCCESS;
+                Console.Error.WriteLine(@"RdRand [Amount]
+Generates random bytes using the CPU RdRand instruction
+
+Amount    - Number of bytes to generate. Writes to stdout
+
+Won't generate any bytes if no argument is supplied.
+Exit codes:
+0 = Success
+1 = RDRAND is not supported
+2 = Help shown
+3 = Invalid command line argument");
+                return EXIT.HELP;
             }
-            return EXIT.NO_RDRAND;
+            else if (args.Length == 0)
+            {
+                return RdRand_Interop.IsRdRandSupported ? EXIT.SUCCESS : EXIT.NO_RDRAND;
+            }
+            else
+            {
+                if (RdRand_Interop.IsRdRandSupported)
+                {
+                    if (args.Length == 1)
+                    {
+                        int count = 0;
+                        if (int.TryParse(args[0], out count) && count > 0)
+                        {
+                            var OUT = Console.OpenStandardOutput();
+                            for (var i = 0; i < count; i += BUFFER)
+                            {
+                                OUT.Write(RdRand_Interop.GetRandom(Math.Min(BUFFER, count - i)), 0, Math.Min(BUFFER, count - i));
+                            }
+                            return EXIT.SUCCESS;
+                        }
+                    }
+                    return EXIT.INVALID_ARGS;
+                }
+                return EXIT.NO_RDRAND;
+            }
         }
     }
 }
