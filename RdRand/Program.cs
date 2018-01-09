3d78748ff19b354b8ff7bed0e20a9e3081fe9b23
Kevin Gut <kevin.gut@kauz.ch>
2018-01-09 15:19:45 +0100

    More debugging

--- RdRand/Program.cs
+++ RdRand/Program.cs
@@ -28,17 +28,22 @@ namespace RdRand
 Generates random bytes using the CPU RdRand instruction
 
 Amount    - Number of bytes to generate. Writes to stdout
+            Runs forever if -1 is supplied.
 
 Won't generate any bytes if no argument is supplied.
 Exit codes:
-0 = Success
-1 = RDRAND is not supported
-2 = Help shown
-3 = Invalid command line argument");
+{0} = Success
+{1} = RDRAND is not supported
+{2} = Help shown
+{3} = Invalid command line argument",
+EXIT.SUCCESS, EXIT.NO_RDRAND, EXIT.HELP, EXIT.INVALID_ARGS);
                 return EXIT.HELP;
             }
             else if (args.Length == 0)
             {
+#if DEBUG
+                Console.Error.WriteLine("RDRAND Supported: {0}", RdRand_Interop.IsRdRandSupported ? "Yes" : "No");
+#endif
                 return RdRand_Interop.IsRdRandSupported ? EXIT.SUCCESS : EXIT.NO_RDRAND;
             }
             else
@@ -48,14 +53,29 @@ Exit codes:
                     if (args.Length == 1)
                     {
                         int count = 0;
-                        if (int.TryParse(args[0], out count) && count > 0)
+                        if (int.TryParse(args[0], out count) && count >= 0)
                         {
-                            var OUT = Console.OpenStandardOutput();
-                            for (var i = 0; i < count; i += BUFFER)
+                            using (var OUT = Console.OpenStandardOutput())
                             {
-                                OUT.Write(RdRand_Interop.GetRandom(Math.Min(BUFFER, count - i)), 0, Math.Min(BUFFER, count - i));
+                                for (var i = 0; i < count; i += BUFFER)
+                                {
+#if DEBUG
+                                    Console.Error.Write('.');
+#endif
+                                    OUT.Write(RdRand_Interop.GetRandom(Math.Min(BUFFER, count - i)), 0, Math.Min(BUFFER, count - i));
+                                }
+                                return EXIT.SUCCESS;
                             }
-                            return EXIT.SUCCESS;
+                        }
+                        else if (count == -1)
+                        {
+                            using (var OUT = Console.OpenStandardOutput())
+                            {
+                                while (true)
+                                {
+                                    OUT.Write(RdRand_Interop.GetRandom(BUFFER), 0, BUFFER);
+                                }
+                            }
                         }
                     }
                     return EXIT.INVALID_ARGS;
